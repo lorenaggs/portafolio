f430ca5077240fb872430b77014dc85dedd23fe8
Leonardo Medina <ext_lpmedina@falabella.cl>
2022-05-14 11:48:21 -0500

    init changes

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import logo from '../images/logo.svg';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        LORENA GUARTAZACA - WEB DEVELOPER PORTAFOLIO
+      </p>
+      <span>
+          LOADING . . .
+      </span>
+    </div>
+  );
+}
+
+export default App;
