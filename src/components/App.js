c3fb85e9d301a33200dcf5b9c8faa6d570ab31d2
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-05-21 17:30:51 -0500

    add image and menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import banner from '../images/banner.png';
-import '../styles/App.scss';
+import "../styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <img src={banner} className="App-logo" alt="logo" />
+    <div className="containerApp">
+      <div className="App">
+        <header className="header">
+          <h1 className="name">Lorena</h1>
+          <h2 className="lastName">Guartazaca</h2>
+        </header>
+        <main className="main">
+          <section className="main_imageprofile">
+            <div className="imageMe"></div>
+          </section>
 
-
+          <p className="main__description">
+            I am
+            <p>&#160;a creative</p>
+            <p>frontend Developer</p>
+            <span className="write">&#160;</span>
+          </p>
+        </main>
+      </div>
+      <ul className="menu">
+        <li className="menu__sections">contact</li>
+        <li className="menu__sections">portafolio</li>
+        <li className="menu__sections">experience</li>
+        <li className="menu__sections">skills</li>
+        <li className="menu__sections">about_me</li>
+      </ul>
     </div>
   );
 }
