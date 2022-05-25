5874ed95608221e1625983a9253a87c9b81fb1c1
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-05-25 17:15:11 -0500

    index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import "./styles/Index.scss";
+import "./styles/index.scss";
 import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
