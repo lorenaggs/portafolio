09f7c9743d8233aa29f8ba18bb5932d30fe2350b
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-09-22 15:55:46 -0500

    send mail

--- src/components/contacme.js
+++ src/components/contacme.js
@@ -1,4 +1,19 @@
+import React from "react";
+import emailjs from "@emailjs/browser";
+
 function contactme() {
+  const sendMail = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_6w7bzyc",
+        "template_6zeftdv",
+        event.target,
+        "T4W92TuixXAZ2VZTT"
+      )
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
   return (
     <section className="contactme" id="contactme">
       {/* <h2 className="title">Contact me</h2> */}
@@ -12,22 +27,28 @@ function contactme() {
         </div>
       </div>
 
-      <form action="" className="form">
+      <form action="" className="form" onSubmit={sendMail}>
         <div className="labelform">
           <label htmlFor="" placeholder="ej. Maria Sanchez" className="label">
             Nombre completo
           </label>
-          <input type="text" className="inputext" />
+          <input type="text" className="inputext" name="user_name" required />
         </div>
         <div className="labelform">
           <label htmlFor="" placeholder="ej. mariasanchez@hotmail.com">
             Email
           </label>
-          <input type="email" className="inputext" />
+          <input type="email" className="inputext" name="user_email" required />
         </div>
         <div className="labelform">
           <label htmlFor="">Escribe un mensaje</label>
-          <textarea type="text" className="inputext" rows="10" />
+          <textarea
+            type="text"
+            className="inputext"
+            rows="10"
+            name="user_message"
+            required
+          />
         </div>
         <button className="btnsent">Enviar</button>
       </form>
